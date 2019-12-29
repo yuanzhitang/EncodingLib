dbbe1b68634bb1895eced81ee03f8aac8f7e49e0
tangym <yuanzhitang@qq.com>
2019-12-29 10:07:21 +0800

    Convert char to UnicodeCodePoint and ConvertUnicodeCodePoint to a real character

---
+++ EncodingLib/EncodingLib/EncodingUtil.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodingLib
+{
+	public class EncodingUtil
+	{
+		/// <summary>
+		/// Convert Char to Unicode Code Point
+		/// For e.g Character "Š", Unicode Code page is \u0160
+		/// </summary>
+		/// <param name="character"></param>
+		/// <returns></returns>
+		public static string ConvertToUnicodeCodePoint(char character)
+		{
+			var bytes = Encoding.Unicode.GetBytes(new char[] { character });
+
+			var lowerByte = bytes[0];
+			var uppperByte = bytes[1];
+
+			string codepoint = uppperByte.ToString("X2")+lowerByte.ToString("X2");
+			return codepoint;
+		}
+
+		/// <summary>
+		/// Convert Char to Unicode Code Point
+		/// For e.g Unicode Code page is \u0160, The output is Character "Š"
+		/// </summary>
+		/// <param name="character"></param>
+		/// <returns></returns>
+		public static char ConvertUnicodeCodePointToChar(string unicodeCodePoint)
+		{
+			var lowerByte = Convert.ToByte(unicodeCodePoint.Substring(0, 2), 16);
+			var uppperByte = Convert.ToByte(unicodeCodePoint.Substring(2, 2), 16);
+			var bytes = new byte[] { uppperByte, lowerByte};
+
+			return Encoding.Unicode.GetChars(bytes)[0];
+		}
+	}
+}
---
+++ EncodingLib/EncodingLib/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodingLib
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Char "Š", Unicode Code page is \u0160
+			string str = "\u0160";
+			string utf16CodePoint = EncodingUtil.ConvertToUnicodeCodePoint(str[0]);
+
+			var character = EncodingUtil.ConvertUnicodeCodePointToChar("0160");
+		}
+	}
+}
