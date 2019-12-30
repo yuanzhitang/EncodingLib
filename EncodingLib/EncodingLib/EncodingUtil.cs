988595f6504c8d771db37eb219c808c9a1b727b6
tangym <yuanzhitang@qq.com>
2019-12-30 15:04:58 +0800

    Add UT projects and Refine the algorithm for UnicodeConvert

--- EncodingLib/EncodingLib/EncodingUtil.cs
+++ EncodingLib/EncodingLib/EncodingUtil.cs
@@ -16,13 +16,15 @@ namespace EncodingLib
 		/// <returns></returns>
 		public static string ConvertToUnicodeCodePoint(char character)
 		{
-			var bytes = Encoding.Unicode.GetBytes(new char[] { character });
+			//var bytes = Encoding.Unicode.GetBytes(new char[] { character });
 
-			var lowerByte = bytes[0];
-			var uppperByte = bytes[1];
+			//var lowerByte = bytes[0];
+			//var uppperByte = bytes[1];
 
-			string codepoint = uppperByte.ToString("X2")+lowerByte.ToString("X2");
-			return codepoint;
+			//string codepoint = uppperByte.ToString("X2") + lowerByte.ToString("X2");
+			//return codepoint;
+
+			return string.Format("{0:x4}", (int)character);
 		}
 
 		/// <summary>
@@ -33,11 +35,13 @@ namespace EncodingLib
 		/// <returns></returns>
 		public static char ConvertUnicodeCodePointToChar(string unicodeCodePoint)
 		{
-			var lowerByte = Convert.ToByte(unicodeCodePoint.Substring(0, 2), 16);
-			var uppperByte = Convert.ToByte(unicodeCodePoint.Substring(2, 2), 16);
-			var bytes = new byte[] { uppperByte, lowerByte};
+			//var lowerByte = Convert.ToByte(unicodeCodePoint.Substring(0, 2), 16);
+			//var uppperByte = Convert.ToByte(unicodeCodePoint.Substring(2, 2), 16);
+			//var bytes = new byte[] { uppperByte, lowerByte};
 
-			return Encoding.Unicode.GetChars(bytes)[0];
+			//return Encoding.Unicode.GetChars(bytes)[0];
+
+			return (char)int.Parse(unicodeCodePoint, System.Globalization.NumberStyles.HexNumber);
 		}
 
 		public static byte[] ConvertHexStringToByteArray(string hex)
@@ -46,6 +50,17 @@ namespace EncodingLib
 					.Where(x => x % 2 == 0)
 					.Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
 					.ToArray();
+		}
+
+		public static string StringToUnicode(string source)
+		{
+			char[] cs = source.ToCharArray();
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < cs.Length; i++)
+			{
+				sb.AppendFormat("\\u{0:x4}", (int)cs[i]);
+			}
+			return sb.ToString();
 		}
 	}
 }
